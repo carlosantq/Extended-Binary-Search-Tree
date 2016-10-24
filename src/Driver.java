e9b611ad8020317d92df6655cf170fb0e2e481fd
carlosantq <carlosantonio.o.n@outlook.com>
2016-10-24 00:43:27 -0300

    read input file

--- src/Driver.java
+++ src/Driver.java
@@ -1,34 +1,49 @@
+import java.util.*;
+import java.io.*;
 
 public class Driver {
-
 	public static void main(String[] args) {
-		
-		System.out.println("Foi!");
-		
-		No raiz = new No(30);
-		raiz.adicionar(10);
-		raiz.adicionar(50);
-		raiz.adicionar(70);
-		raiz.adicionar(1);
-//		raiz.adicionar(20);
-//		raiz.adicionar(40);
-		raiz.adicionar(0);
-//		raiz.adicionar(5);
-//		raiz.adicionar(15);
-//		raiz.adicionar(25);
-//		raiz.adicionar(35);
-//		raiz.adicionar(45);
-//		raiz.adicionar(60);
-//		raiz.adicionar(100);
-		
-		System.out.println("");
-		
-		System.out.println("Teste do toString():");
-		System.out.println(raiz.toString());
-		
-		System.out.println("");
-		
-		raiz.print();
-	}
 
+		//Árvore
+		No arvore = null;
+		int numero;
+
+		if (args.length < 1){
+			System.out.println("Erro! Arquivo de entrada não informado.");
+		}
+
+		try{
+			//Abrindo o arquivo passado via argumento
+			FileReader fr = new FileReader(args[0]);
+			BufferedReader br = new BufferedReader(fr);
+
+			//Considerando que apenas uma linha do arquivo será levada em conta
+			String linha = br.readLine();
+
+			// while (linha != null){
+				System.out.println("Linha: " + linha);
+
+				//Primeiro número é a raíz da árvore
+				numero = Integer.parseInt(linha.substring(0, linha.indexOf(" ")));
+				arvore = new No(numero);
+
+				linha = linha.substring(linha.indexOf(" ")+1, linha.length());
+
+				//Adicionando números do arquivo restantes na árvore criada anteriormente
+				while (linha.length() > 0){
+					numero = Integer.parseInt(linha.substring(0, linha.indexOf(" ")));
+					arvore.adicionar(numero);
+					linha = linha.substring(linha.indexOf(" ")+1, linha.length());
+				}
+
+				// linha = br.readLine();
+			// }
+			fr.close();
+		}catch (IOException ioe){
+			System.out.println("Erro na abertura do arquivo: " + ioe.getMessage());
+		}
+
+		//Imprimindo a árvore (depuração)
+		System.out.println(arvore.toString());
+	}
 }
