8d30343fab040ed6ffcf59fa58d315abab073221
Pedro H. B. Cavalcante <pedrinho.sjs@ufrn.edu.br>
2016-10-24 15:27:22 -0300

    Adição do JFileChooser
    Segue da seguinte maneira: Na primeira janela que abre, escolhe o arquivo de entrada. Na segunda Janela, escolhe o arquivo de instruções. Estou pensando em adicionar dois confirmDialog para que ela saiba o que deve fazer :)

--- src/Driver.java
+++ src/Driver.java
@@ -1,103 +1,165 @@
-import java.util.*;
+package binaryTree;
+
+import javax.swing.*;
+
+import java.awt.Component;
 import java.io.*;
+	
+	
+	
+	public class Driver{
+		
+	
+		final static JFileChooser chooser = new JFileChooser();
+		static Component parent = null;
+		
+		public static void main(String[] args) {
+			
+			//árvore
+			No arvore = null;
+			int numero = 0;
+			String operacao = null;
+			int parametro = 0;
+			//ENTRADA
 
-public class Driver {
-	public static void main(String[] args) {
+			try{
+				chooser.setMultiSelectionEnabled(true);
+				
+				try{
+					chooser.showOpenDialog(parent);
+				}catch (NullPointerException e){
+					System.out.println(e.getStackTrace());
+				}
+				
+				
+				//Abrindo o arquivo passado via argumento
+				FileReader fr = null;
+				BufferedReader br = null;
+				try{
+					fr = new FileReader(chooser.getSelectedFile());
+					br = new BufferedReader(fr);
+				}catch (NullPointerException e){
+					System.out.println(e.getStackTrace());
+				}
+				
+				String linha = "";
 
-		//Árvore
-		No arvore = null;
-		int numero;
-		String operacao;
-		int parametro = 0;
+				//Considerando que apenas uma linha do arquivo será levada em conta
+				try{
+					 linha = br.readLine();
+					 System.out.println("Linha: " + linha);
+				}catch (NullPointerException e){
+					System.out.println(e.getMessage());
+				}
+				
 
-		if (args.length < 2){
-			System.out.println("Erro! Arquivos de entrada não informados.");
-		}
+				// while (linha != null){
+					
 
-		//ENTRADA
+					//Primeiro número da a raíz da Árvore
+					try{
+						numero = Integer.parseInt(linha.substring(0, linha.indexOf(" ")));
+					}catch (StringIndexOutOfBoundsException e){
+						System.out.println(e.getStackTrace());
+					}
+					
+					arvore = new No(numero);
 
-		try{
-			//Abrindo o arquivo passado via argumento
-			FileReader fr = new FileReader(args[0]);
-			BufferedReader br = new BufferedReader(fr);
-
-			//Considerando que apenas uma linha do arquivo será levada em conta
-			String linha = br.readLine();
-
-			// while (linha != null){
-				System.out.println("Linha: " + linha);
-
-				//Primeiro número é a raíz da árvore
-				numero = Integer.parseInt(linha.substring(0, linha.indexOf(" ")));
-				arvore = new No(numero);
-
-				linha = linha.substring(linha.indexOf(" ")+1, linha.length());
-
-				//Adicionando números do arquivo restantes na árvore criada anteriormente
-				while (linha.length() > 0){
-					numero = Integer.parseInt(linha.substring(0, linha.indexOf(" ")));
-					arvore.adicionar(numero);
 					linha = linha.substring(linha.indexOf(" ")+1, linha.length());
+
+					//Adicionando números do arquivo restantes na árvore criada anteriormente
+					while (linha.length() > 0){
+						numero = Integer.parseInt(linha.substring(0, linha.indexOf(" ")));
+						arvore.adicionar(numero);
+						linha = linha.substring(linha.indexOf(" ")+1, linha.length());
+					}
+
+					// linha = br.readLine();
+				// }
+				try{
+					fr.close();
+				}catch (NullPointerException e){
+					System.out.println(e.getStackTrace());
 				}
-
-				// linha = br.readLine();
-			// }
-			fr.close();
-		}catch (IOException ioe){
-			System.out.println("Erro na abertura do arquivo: " + ioe.getMessage());
-		}
-
-
-		//INSTRUÇÕES
-
-		try{
-			FileReader fr = new FileReader(args[1]);
-			BufferedReader br = new BufferedReader(fr);
-
-			String linha = br.readLine();
-
-			while (linha != null){
-				System.out.println("Linha: " + linha);
-
-				operacao = linha.substring(0, linha.indexOf(" "));
-
-				if (operacao.equals("ENESIMO") || operacao.equals("POSICAO") || operacao.equals("REMOVA")){
-					linha = linha.substring(linha.indexOf(" ")+1, linha.length());
-					parametro = Integer.parseInt(linha.substring(0, linha.length()));
-				}
-
-				switch(operacao){
-					case "IMPRIMA":
-						System.out.println(arvore.toString());
-						break;
-					case "ENESIMO":
-						System.out.println(arvore.enesimoElemento(parametro));
-						break;
-					case "POSICAO":
-						System.out.println(arvore.posicao(parametro));
-						break;
-					case "REMOVA":
-						System.out.println(arvore.remover(parametro) ? "Removido o valor " + parametro : "O valor não pôde ser removido.");
-						break;
-					case "MEDIANA":
-						System.out.println(arvore.mediana());
-						break;
-					case "CHEIA":
-						System.out.println(arvore.ehCheia() ? "É Cheia" : "Não é cheia");
-						break;
-					case "COMPLETA":
-						System.out.println(arvore.ehCompleta() ? "É Completa" : "Não é completa");
-						break;	
-				}
-
-				linha = br.readLine();
-
+			}catch (IOException ioe){
+				System.out.println("Erro na abertura do arquivo: " + ioe.getMessage());
 			}
-		}catch (IOException ioe){
-			System.out.println("Erro na abertura do arquivo: " + ioe.getMessage());
-		}
 
-		arvore.print();
+
+			//INSTRUÃ‡Ã•ES
+
+			try{
+				chooser.showOpenDialog(parent);
+				FileReader fr;
+				BufferedReader br = null;
+				try{
+					fr = new FileReader(chooser.getSelectedFile());
+					br = new BufferedReader(fr);
+				}catch (NullPointerException e){
+					System.out.println("error");
+				}
+				
+				String linha = "";
+				try{
+					linha  = br.readLine();
+				}catch (NullPointerException e){
+					System.out.println("error");
+				}
+
+				while (linha != null){
+					System.out.println("Linha: " + linha);
+
+					try{
+						operacao = linha.substring(0, linha.indexOf(" "));
+					}catch (StringIndexOutOfBoundsException e){
+						System.out.println("erro");
+					}
+
+					if (operacao == "ENESIMO" || operacao == "POSICAO" || operacao == "REMOVA"){
+						linha = linha.substring(linha.indexOf(" ")+1, linha.length());
+						parametro = Integer.parseInt(linha.substring(0, linha.length()));
+					}
+
+					try{
+						switch(operacao){
+					
+						case "IMPRIMA":
+							System.out.println(arvore.toString());
+							break;
+						case "ENESIMO":
+							System.out.println(arvore.enesimoElemento(parametro));
+							break;
+						case "POSICAO":
+							System.out.println(arvore.posicao(parametro));
+							break;
+						case "REMOVA":
+							System.out.println(arvore.remover(parametro) ? "Removido o valor " + parametro : "O valor não pode ser removido.");
+							break;
+						case "MEDIANA":
+							System.out.println(arvore.mediana());
+							break;
+						case "CHEIA":
+							System.out.println(arvore.ehCheia() ? "É Cheia" : "Não é cheia");
+							break;
+						case "COMPLETA":
+							System.out.println(arvore.ehCompleta() ? "É Completa" : "Não é completa");
+							break;	
+					}
+					}catch (NullPointerException e){
+						System.out.println("erro");
+					}
+					try{
+						linha = br.readLine();
+					}catch(NullPointerException e){
+						System.out.println("erro");
+						break;
+					}
+
+				}
+			}catch (IOException ioe){
+				System.out.println("Erro na abertura do arquivo: " + ioe.getMessage());
+			}
+
+		}
 
 	}
-}
